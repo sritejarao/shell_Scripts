679e4d0ec004fbe709bff84f1652c24671cdfba9
Sri Teja Pulugujju <spulugujju@zetaglobal.com>
2025-01-15 12:58:43 +0530

    Passing values outside the shell

--- 04-variable_add_condition.sh
+++ 04-variable_add_condition.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-#basic variable passing outside the shell script
-y=$2
-x=$1
+#Skip the script if values are empty.
 
+x=$1
+y=$2
+
+if[-n "$x"] && [-n "$y"] then
 echo "$y -> Hello $x"
 echo "$x -> Hi $y"
 echo "${y} -> how are you doing $x"
 echo "$x - doing great and u?"
 echo "$y -> Mee too"
+
+else
+echo "values are empty"
