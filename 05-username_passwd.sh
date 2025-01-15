38b6c0940559f9bfac86f465c96b84d784cdebcf
Sri Teja Pulugujju <spulugujju@zetaglobal.com>
2025-01-15 14:29:46 +0530

    Passing values outside the shell_rules

---
+++ 05-username_passwd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#To read username and password from command line.
+
+echo "Please enter the username"
+read -s USERNAME
+echo "username entered" $USERNAME
+
+echo "Please enter password"
+read -s PASSWORD
