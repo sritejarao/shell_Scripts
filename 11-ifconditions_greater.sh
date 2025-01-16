1b00dec522638a40bb76d1e360ea2a2a8f694938
Sri Teja Pulugujju <spulugujju@zetaglobal.com>
2025-01-16 16:04:10 +0530

    canges

--- 11-ifconditions_greater.sh
+++ 11-ifconditions_greater.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-num=1;
-num2=2;
+num=1
+num2=2
 
 if [ $num -gt $num2 ]; then
-echo "${num} is greater than ${num2}"
+    echo "${num} is greater than ${num2}"
 else
-echo "${num} is smaller than ${num2}"
+    echo "${num} is smaller than ${num2}"
 fi
