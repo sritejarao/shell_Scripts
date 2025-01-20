b6450b623412ba61238f9761ae0c70a260265b80
Sri Teja Pulugujju <spulugujju@zetaglobal.com>
2025-01-20 08:17:20 +0530

    latest changes

--- 11-ifconditions_greater.sh
+++ 11-ifconditions_greater.sh
@@ -4,7 +4,7 @@ num=1
 num2=2
 
 if [ $num -gt $num2 ]; then
-    echo "${num} is greater than ${num2}"
+echo "${num} is greater than ${num2}"
 else
-    echo "${num} is smaller than ${num2}"
+echo "${num} is a smaller than ${num2}"
 fi
