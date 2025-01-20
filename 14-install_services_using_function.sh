71b901d41354ef356aeffe315d64a14a31352ca2
Sri Teja Pulugujju <spulugujju@zetaglobal.com>
2025-01-20 16:41:12 +0530

    latest changes

---
+++ 14-install_services_using_function.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+VALIDATE()
+{
+    if [ $? -ne 0]; then
+        echo "$2 is not installed properly, check once"
+        exit 1
+    else
+        echo "$2 installation is complete"
+    fi
+}
+
+if [ $USER -ne 0 ]; then
+    echo "To install mysql user shoould have root access"
+    exit 1
+else 
+    echo "This user can only install mysql service"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySql is not installed, starting the installation process"
+    dnf install mysql -y
+
+    VALIDATE $? "MySQl"
+
+else
+    echo "mysql is already installed on this server"
+    exit 1
+fi
+
