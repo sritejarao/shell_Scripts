68f44d48cfe163cf0718fd83293c7b5d5ceb2c38
Sri Teja Pulugujju <spulugujju@zetaglobal.com>
2025-01-20 16:24:29 +0530

    latest changes

--- 13-install_usingconditions.sh
+++ 13-install_usingconditions.sh
@@ -13,10 +13,18 @@ dnf list installed mysql
 
 if [ $? -ne 0 ]
 then
-    echo "MySql is not installed"
+    echo "MySql is not installed, starting the installation process"
+    dnf install mysql -y
+    
+    if [ $? -ne 0]; then
+        echo "mysql is not installed properly, check once"
+        exit 1
+    else
+        echo "mySql installation is complete"
+    fi
+
 else
     echo "mysql is installed on this server"
+    exit 1
 fi
-
-#dnf install mysql -y
 
