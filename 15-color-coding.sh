8e28746da1c4a8db36f952e913da3b80990c9772
Sri Teja Pulugujju <spulugujju@zetaglobal.com>
2025-01-20 17:17:50 +0530

    latest changes

---
+++ 15-color-coding.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USER=$(id -u)
+R="\e[31m"
+G=\e[32m"
+
+VALIDATE()
+{
+    if [ $? -ne 0 ]; then
+        echo -e " $R $2 is not installed, check once"
+        exit 1
+    else
+        echo  -e "$G $2 installation is complete"
+    fi
+}
+
+if [ $USER -ne 0 ]; then
+    echo "To install mysql user should have root access"
+    exit 1
+else 
+    echo -e "$G This user can only install mysql service"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo  "MySql is not installed, starting the installation process"
+    dnf install mysql -y
+
+    VALIDATE $? "MySQl"
+
+else
+    echo "mysql is already installed on this server"
+    
+fi
+
+dnf list installed git
+VALIDATE $? "Git"
+
+dnf list installed ngnix
+VALIDATE $? "ngnix"
+
+exit 0
